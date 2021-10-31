754655757a7177468c9ea11a694bd132ace0dc3c
Solaimanur Rahman <rahmansolaiman@gmail.com>
2021-10-31 13:42:27 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,42 @@
+document.getElementById("storagebutton").addEventListener("click", function () {
+      // Set Input Value
+    const cartCount = document.getElementById("storage");
+  // Addition To Input Filed Value + Set Value
+  cartCount.innerText =  100;
+  });
+  document.getElementById("storagebutton1").addEventListener("click", function () {
+    // Set Input Value
+  const cartCount = document.getElementById("storage");
+// Addition To Input Filed Value + Set Value
+cartCount.innerText =  0;
+});
+document.getElementById("storagebutton2").addEventListener("click", function () {
+    // Set Input Value
+  const cartCount = document.getElementById("storage");
+// Addition To Input Filed Value + Set Value
+cartCount.innerText =  180;
+});
+document.getElementById("memorybutton").addEventListener("click", function () {
+    // Set Input Value
+  const cartCount = document.getElementById("memory");
+// Addition To Input Filed Value + Set Value
+cartCount.innerText =  180;
+});
+document.getElementById("memorybutton1").addEventListener("click", function () {
+    // Set Input Value
+  const cartCount = document.getElementById("memory");
+// Addition To Input Filed Value + Set Value
+cartCount.innerText =  0;
+});
+document.getElementById("deliverybutton").addEventListener("click", function () {
+    // Set Input Value
+  const cartCount = document.getElementById("delivery");
+// Addition To Input Filed Value + Set Value
+cartCount.innerText =  20;
+});
+document.getElementById("deliverybutton1").addEventListener("click", function () {
+    // Set Input Value
+  const cartCount = document.getElementById("delivery");
+// Addition To Input Filed Value + Set Value
+cartCount.innerText =  0;
+});
